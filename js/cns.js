21bfa98b54be6718746a09b08e4e91d2cad99727
Dillon Mendes <djmendes4@gmail.com>
2017-05-19 08:43:48 -0700

    Update cns.js

--- js/cns.js
+++ js/cns.js
@@ -6,8 +6,8 @@ this.handler = new Handler();
 
 this.transition.initialize(this.landscape);
 this.transition.setTransition(this.transition.transitionTypes.linear);
-this.transition.setHorizontalCenter(74);
-this.transition.setVerticalCenter(34);
+this.transition.setHorizontalCenter(Math.floor(Math.random() * neon.columns));
+this.transition.setVerticalCenter(Math.floor(Math.random() * neon.rows));
 this.transition.setColor('grey');
 this.transition.setAngle(0);
 this.transition.setVariance(0.1);
